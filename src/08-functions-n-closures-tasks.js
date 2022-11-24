79906400c26503e3c5b254a54614c75b9d096f1f
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-11-24 22:43:04 +0300

    feat: implement tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
 
 
@@ -81,8 +81,12 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache;
+  return () => {
+    if (!cache) cache = func();
+    return cache;
+  };
 }
 
 
@@ -101,8 +105,20 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let currentAttempts = attempts;
+  return () => {
+    let value;
+    while (currentAttempts) {
+      try {
+        value = func();
+        return value;
+      } catch (error) {
+        currentAttempts -= 1;
+      }
+    }
+    return value;
+  };
 }
 
 
@@ -169,8 +185,17 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let flag;
+  let id = startFrom;
+  return () => {
+    if (!flag) {
+      flag = true;
+      return id;
+    }
+    id += 1;
+    return id;
+  };
 }
 
 
