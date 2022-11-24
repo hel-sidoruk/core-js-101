e13d3d31d81e1f8a7bf861879f00cbcd28908871
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-11-24 23:31:42 +0300

    feat: implement tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -145,8 +145,12 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (val) => {
+    logFunc(`${func.name}(${val}) starts`);
+    logFunc(`${func.name}(${val}) ends`);
+    return func(val);
+  };
 }
 
 
@@ -163,8 +167,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn(...[...args1, ...args2]);
 }
 
 
